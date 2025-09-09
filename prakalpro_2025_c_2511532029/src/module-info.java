47946ed0ef6fbaa1684699d61c7a8213257bdac8
Poalca <yeahitspoll@gmail.com>
2025-09-09 11:57:13 +0700

    praktikum 1 kelas c

---
+++ prakalpro_2025_c_2511532029/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author FTI-LAB03-PC17
+ *
+ */
+module prakalpro_2025_c_2511532029 {
+}
---
+++ prakalpro_2025_c_2511532029/src/pekan1/HelloWorld.java
@@ -0,0 +1,20 @@
+package pekan1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//program pertama
+		/*komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum alpro
+		 */
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Program ini menghasilkan");
+		System.err.println("empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("This"+" program prints a\n"+"quote from the Gettysburg Address.");
+
+	}
+
+}
